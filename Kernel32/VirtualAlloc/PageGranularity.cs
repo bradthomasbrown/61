f72de4bf022221e47d55a29ad4479889ec761db8
Brad Brown <bradthomasbrown@outlook.com>
2025-05-19 18:57:56 -0500

    VirtualAlloc page granularity constant

---
+++ Kernel32/VirtualAlloc/PageGranularity.cs
@@ -0,0 +1,9 @@
+namespace Kernel32 {
+partial class VirtualAlloc {
+
+
+// https://devblogs.microsoft.com/oldnewthing/20031008-00/?p=42223
+public const uint PageGranularity                   = 0x00010000;
+
+
+} }

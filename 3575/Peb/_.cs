10cae76b7e09f6643a21588dd8818f4eebea875b
Brad Brown <bradthomasbrown@outlook.com>
2025-05-19 18:53:54 -0500

    PEB structure wrapper

---
+++ 3575/Peb/_.cs
@@ -0,0 +1,15 @@
+namespace _3575_ {
+public partial class Peb {
+
+
+unsafe public struct _ {
+ public fixed byte  Reserved1[2]            ;
+ public       byte  BeingDebugged           ;
+ public fixed byte  Reserved2[1]            ;
+ public fixed ulong Reserved3[2]            ; 
+ public       ulong LdrAddress              ;
+ public       ulong ProcessParametersAddress;
+}
+
+
+} }

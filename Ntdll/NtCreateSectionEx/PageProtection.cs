97af4dbd809693f70634924107aff2959713f289
Brad Brown <bradthomasbrown@outlook.com>
2025-05-19 19:00:12 -0500

    NtCreateSectionEx PageProtection enum

---
+++ Ntdll/NtCreateSectionEx/PageProtection.cs
@@ -0,0 +1,13 @@
+namespace Ntdll {
+partial class NtCreateSectionEx {
+
+
+public enum PageProtection : uint {
+ PAGE_READONLY  = 0x00000002,
+ PAGE_READWRITE = 0x00000004,
+ PAGE_WRITECOPY = 0x00000008,
+ PAGE_EXECUTE   = 0x00000010 
+}
+
+
+} }

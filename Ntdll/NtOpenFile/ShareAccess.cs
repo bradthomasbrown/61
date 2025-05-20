506983271a6cc78ca725a001b20d2b4514c4641c
Brad Brown <bradthomasbrown@outlook.com>
2025-05-19 19:02:31 -0500

    NtOpenFile ShareAccess enum

---
+++ Ntdll/NtOpenFile/ShareAccess.cs
@@ -0,0 +1,12 @@
+namespace Ntdll {
+partial class NtOpenFile {
+
+
+public enum ShareAccess : uint {
+ FILE_SHARE_READ   = 0x00000001,
+ FILE_SHARE_WRITE  = 0x00000002,
+ FILE_SHARE_DELETE = 0x00000004,
+}
+
+
+} }

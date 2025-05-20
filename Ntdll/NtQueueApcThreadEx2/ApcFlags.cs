aa92bcd2aecedf983172b0f0facc679491882951
Brad Brown <bradthomasbrown@outlook.com>
2025-05-19 19:03:11 -0500

    NtQueueApcThreadEx2 ApcFlags enum

---
+++ Ntdll/NtQueueApcThreadEx2/ApcFlags.cs
@@ -0,0 +1,11 @@
+namespace Ntdll {
+partial class NtQueueApcThreadEx2 {
+
+
+public enum ApcFlags : uint {
+ QUEUE_USER_APC_FLAGS_SPECIAL_USER_APC      = 0x00000001,
+ QUEUE_USER_APC_FLAGS_CALLBACK_DATA_CONTEXT = 0x00010000,
+}
+
+
+} }

e76db8bf7141e40de57e7cd51c5c9c59f4f702c2
Brad Brown <bradthomasbrown@outlook.com>
2025-05-19 19:00:53 -0500

    NtCreateSectionEx AllocationType enum

---
+++ Ntdll/NtMapViewOfSection/AllocationType.cs
@@ -0,0 +1,14 @@
+namespace Ntdll {
+partial class NtMapViewOfSection {
+
+
+public enum AllocationType : uint {
+ MEM_RESERVE                 = 0x00002000,
+ MEM_REPLACE_PLACEHOLDER     = 0x00004000,
+ MEM_TOP_DOWN                = 0x00100000,
+ MEM_DIFFERENT_IMAGE_BASE_OK = 0x00800000,
+ MEM_LARGE_PAGES             = 0x20000000
+}
+
+
+} }
